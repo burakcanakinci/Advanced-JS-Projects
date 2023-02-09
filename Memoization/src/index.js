0f3b7ad2fa87434c35d219ffcec4ef3cae407a40
Burak Can Akıncı <36580021+burakcanakinci@users.noreply.github.com>
2023-02-09 15:58:24 +0200

    Update index.js

--- Memoization/src/index.js
+++ Memoization/src/index.js
@@ -1,7 +1,4 @@
-// Memoize
-
 const defaultResolver = (...args) => JSON.stringify(args);
-
 export function memoize(fn, resolver = defaultResolver) {
   const cache = new Map();
   const memoized = (...args) => {
@@ -24,37 +21,3 @@ export function memoize(fn, resolver = defaultResolver) {
   }
   return memoized;
 }
-
-// export function memoize(fn) {
-//   const cache = new Map();
-  
-//   const memoized = (...args) => {
-//     const key = JSON.stringify(args);
-
-//     if (cache.has(key)) {
-//       return cache.get(key);
-//     }
-
-//     const result = fn(...args);
-//     cache.set(key, result);
-
-//     return result;
-//   };
-//   // memoized.has = () => if (cache.has(key)) {
-//   //   return cache.get(key);
-//   // };
-//   memoized.clear = () => memoized.cache.clear();
-//   // memoized.delete = () => memo.delete();
-//   return memoized;
-// }
-
-
-// get all the arguments and "stringify" them to build the key of the cache object
-// verify if the key exists in the cache.
-// If it does
-// return the output value
-// if it doesn't
-// call the function
-// store the output value in the cache
-// and return the output value
-
